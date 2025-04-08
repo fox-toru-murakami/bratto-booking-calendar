980c0e8374de1757ce9772dba4326b0e4c3cda74
toru murakami <toru.murakami@fox-hound.jp>
2025-04-08 19:21:43 +0900

    deploy.yml変更

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'bratto-booking-calendar'
+
 const nextConfig: NextConfig = {
   output: 'export',  // 静的ファイルをエクスポート
-  basePath: process.env.NODE_ENV === 'production' ? '/bratto-booking-calendar' : '',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '/',
+  // URLの末尾にスラッシュを追加
+  trailingSlash: true,
+  // GitHub Pagesのための画像最適化を無効化
   images: {
-    unoptimized: true,  // GitHub Pagesで必要
-  },
-  trailingSlash: true,  // URL末尾にスラッシュを追加（GitHub Pages用）
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
